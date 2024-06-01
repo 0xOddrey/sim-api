ec62bca5cf5f3ce66587f19a853ed531593313eb
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-01 17:13:08 -0500

    updates

--- api/words/index.py
+++ api/words/index.py
@@ -1,38 +1,50 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import spacy
 import json
+import fasttext
+import fasttext.util
 
-nlp = spacy.load("en_core_web_sm", disable=["tagger", "attribute_ruler", "lemmatizer"])
-
+# Download and load FastText English vectors
+fasttext.util.download_model('en', if_exists='ignore')
+ft = fasttext.load_model('cc.en.300.bin')
 
 class handler(BaseHTTPRequestHandler):
 
-	def _set_headers(self):
-		self.send_header('Content-Type', 'application/json')
-		self.send_header('Access-Control-Allow-Origin', '*')  # Allow all origins
-		self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-		self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+    def _set_headers(self):
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')  # Allow all origins
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
 
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self._set_headers()
+        self.end_headers()
 
-	def do_OPTIONS(self):
-		self.send_response(200)
-		self._set_headers()
-		self.end_headers()
+    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 
+        word = dic["word"]
+        answer = dic['answer']
 
-	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-	
-		word = dic["word"]
-		answer = dic['answer']
-		print(word)
-		sim = nlp(answer).similarity(nlp(word))
-		score =  sim * 100
-		result = json.dumps({"score": score})
-		self.send_response(200)
-		self._set_headers()
-		self.end_headers()
-		self.wfile.write(result.encode())
-		return
+        # Get vectors from FastText
+        word_vector = ft.get_word_vector(word)
+        answer_vector = ft.get_word_vector(answer)
+
+        # Calculate cosine similarity
+        sim = self.cosine_similarity(answer_vector, word_vector)
+        score = sim * 100
+        result = json.dumps({"score": score})
+
+        self.send_response(200)
+        self._set_headers()
+        self.end_headers()
+        self.wfile.write(result.encode())
+        return
+
+    def cosine_similarity(self, vec1, vec2):
+        dot_product = vec1.dot(vec2)
+        norm_vec1 = (vec1 ** 2).sum() ** 0.5
+        norm_vec2 = (vec2 ** 2).sum() ** 0.5
+        return dot_product / (norm_vec1 * norm_vec2)
