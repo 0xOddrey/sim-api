b1be5ce92e00f12d2966bb569f68ca5412abf070
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-02 03:27:53 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import spacy
 import json
+
+
+
+nlp = spacy.load("en_core_web_md")
+
 
 
 class handler(BaseHTTPRequestHandler):
@@ -19,7 +25,33 @@ class handler(BaseHTTPRequestHandler):
 
 
 	def do_GET(self):
-		result = json.dumps({"score": 5})
+		s = self.path
+		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		word = dic["word"]
+		answer = dic['answer']
+		#lower case the words
+		word = word.lower()
+		answer = answer.lower()
+		#check to ensure letters in each word don't overlap by 90% 
+		word_letters = word.split()
+		answer_letters = answer.split()
+		#how to check word like lion and lions are too similar 
+		overlap = 0
+		total = 0
+		for letter in word_letters:
+			if letter in answer_letters:
+				overlap += 1
+			total += 1
+		if overlap/total > 0.9:
+			result = json.dumps({"score": 0})
+			self.send_response(200)
+			self._set_headers()
+			self.end_headers()
+			self.wfile.write(result.encode())
+			return
+		sim = nlp(word).similarity(nlp(answer))
+		score =  sim * 100
+		result = json.dumps({"score": score})
 		self.send_response(200)
 		self._set_headers()
 		self.end_headers()
